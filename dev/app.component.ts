04cd6ca164201a01c9dabe1040a958f4cd33255c
Przemysław Sołtan <przemyslaw.soltan@gmail.com>
2016-03-19 17:55:44 +0100

    add simple components

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,28 +1,40 @@
 import {Component} from 'angular2/core';
 import {ROUTER_DIRECTIVES, RouteConfig, Router} from "angular2/router";
-import {HomeComponent} from "./home/home.component";
-import {AboutComponent} from "./about/about.component";
 import {RouterActiveDirective} from "./directive/router-active.directive";
+import {HomeComponent} from "./home/home.component";
+import {LoginComponent} from "./login/login.component";
+import {RegisterComponent} from "./register/register.component";
+import {AboutComponent} from "./about/about.component";
 import {SettingsComponent} from "./settings/settings.component";
 
 @Component({
     selector: 'app',
     template: `
         <header class="header">
-            <div class="container">
-                <h1 class="title">{{title}}</h1>
+            <div class="container nav">
+                <div class="nav-brand">{{title}}</div>
+                <div class="nav-right">
+                    <a class="btn is-success" [routerLink]="['Register']"><i class="fa fa-user"></i> Register</a>
+                    <a class="btn is-primary" [routerLink]="['Login']"><i class="fa fa-lock"></i> Login</a>
+                    <!--
+                    <a class="btn is-danger" [routerLink]="['Home']"><i class="fa fa-power-off"></i> Logout</a>
+                    -->
+                </div>
             </div>
         </header>
 
         <nav class="is-primary">
-            <ul class="container tabs is-boxed">
+            <div class="container">
+                <ul class="tabs is-boxed">
                 <li router-active><a [routerLink]="['Home']">Home</a></li>
                 <li router-active><a [routerLink]="['About']">About</a></li>
+                <li class="is-space"></li>
                 <li router-active><a [routerLink]="['Settings']">Settings</a></li>
-            </ul>
+                </ul>
+            </div>
         </nav>
 
-        <main class="container">
+        <main class="main container">
             <router-outlet></router-outlet>
         </main>
 
@@ -41,6 +53,8 @@ import {SettingsComponent} from "./settings/settings.component";
 
 @RouteConfig([
     {path: '/home', name: 'Home', component: HomeComponent, useAsDefault: true},
+    {path: '/login', name: 'Login', component: LoginComponent},
+    {path: '/register', name: 'Register', component: RegisterComponent},
     {path: '/about', name: 'About', component: AboutComponent},
     {path: '/settings', name: 'Settings', component: SettingsComponent}
 ])
@@ -48,7 +62,7 @@ export class AppComponent {
 
     title = 'Gedeonix UI';
 
-    constructor(private router: Router) {
+    constructor(private router:Router) {
     }
 
     public isRouteActive(route) {
---
+++ dev/login/login.component.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    template: `
+        <h1>Login</h1>
+        ...
+    `
+})
+export class LoginComponent {
+
+}
